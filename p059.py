c0b2eb0509888ac7078f6df2974d5647f458e4ee
Henry Wallace <hflwallace@gmail.com>
2017-05-07 15:59:26 -0400

    Tweak p059 to actually work

--- p059.py
+++ p059.py
@@ -1,36 +1,72 @@
 from collections import Counter
-from itertools import islice, product, repeat
+from itertools import cycle
 from math import log
 from string import ascii_lowercase
 
-from nltk.corpus import gutenberg
 
-with open('p059_cipher.txt') as f:
-    chars = list(map(int, f.read().split(',')))
-
-# use bigram transition probabilities
-text = gutenberg.raw('whitman-leaves.txt')
-c = Counter(zip(text, islice(text, 1, None)))
-z = sum(c.values())
-reference = {k: v/z for k, v in c.items()}
+def read_cipher(path):
+    """Read the cipher file, and return a list of code points."""
+    with open(path) as f:
+        return [int(cp) for cp in f.read().strip().split(',')]
 
 
-def decrypt(key):
-    return ''.join(chr(ord(key[i % len(key)]) ^ x)
-                   for i, x in enumerate(chars))
+def corpus_counts(path):
+    """Return unigram counts from a corpus."""
+    with open(path) as f:
+        text = f.read()
+    return Counter(text)
 
 
-def likelihood(message, reference):
-    l = 0
-    for bigram in zip(message, islice(message, 1, None)):
-        l += log(reference.get(bigram, 1))
-    return l
+def score(text, counts):
+    """
+    Return a score deciphered text given unigram counts from another corpus.
 
-m, k = float('inf'), None
-for t in product(*repeat(ascii_lowercase, 3)):
-    key = ''.join(t)
-    l = likelihood(decrypt(key), reference)
-    if l < m:
-        m, k = l, key
-print(m, k)
-print(decrypt(k))
+    A higher score indicates a better decipher solution. While we could
+    define this score as the product of probabilities, we realize that
+    everything is normalized the same. So, we can just use counters. And
+    to then avoid overflow, we just do a log sum.
+    """
+    return sum(log(counts.get(c, 1)) for c in text)
+
+
+def partial_decipher(cipher, code_point, i, keylen):
+    """Partially decipher with just one code point of a key."""
+    return ''.join(chr(cp ^ code_point) for j, cp in enumerate(cipher)
+                   if j % keylen == i)
+
+
+def decipher(cipher, key):
+    """Return a complete decipher solution."""
+    partials = [iter(partial_decipher(cipher, cp, i, len(key)))
+                for i, cp in enumerate(key)]
+    return ''.join(map(next, cycle(partials)))
+
+
+def solve(cipher, counts, keylen=3):
+    """
+    Return the highest scoring cipher key.
+
+    We define the best key to be the sequence of highest scoring
+    code points from partial decipher solutions.
+    """
+    best_key = []
+    for i in range(keylen):
+        best_score, best_cp = -1, None
+        for cp in map(ord, ascii_lowercase):
+            text = partial_decipher(cipher, cp, i, keylen)
+            s = score(text, counts)
+            if s > best_score:
+                best_score, best_cp = s, cp
+        best_key.append(best_cp)
+    return best_key
+
+
+if __name__ == '__main__':
+    counts = corpus_counts('p059_ulysses.txt')
+    cipher = read_cipher('p059_cipher.txt')
+    best_key = solve(cipher, counts)
+    deciphered = decipher(cipher, best_key)
+
+    print(''.join(chr(cp) for cp in best_key))
+    print(deciphered)
+    print(sum(ord(c) for c in deciphered))
